3886f596f72b025c26e0def65fccadcc5108b2ba
An041003 <binhan214981@gmail.com>
2025-05-28 22:42:16 +0700

    Update settings.py

--- backend/foodpro_backend/settings.py
+++ backend/foodpro_backend/settings.py
@@ -42,7 +42,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "https://your-frontend-domain.com"
+    "https://your-frontend.vercel.app",
 ]
 
 
@@ -77,8 +77,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, '../frontend/dist/assets')]
+MIDDLEWARE.insert(0, "corsheaders.middleware.CorsMiddleware")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ROOT_URLCONF = 'foodpro_backend.urls'
@@ -145,7 +144,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
